b4eaf92fb5e3239c0d9b2017da720e9efaefbef9
Ahmad Rifki Farid <100522916+Rifkiwastaken@users.noreply.github.com>
2022-11-10 11:23:58 +0700

    Create Elemen.java

---
+++ Elemen.java
@@ -0,0 +1,12 @@
+public class Elemen {
+    public String nama;
+
+    public Elemen(String nama) {
+        /*
+         * merupakan DOWNCASTING,karena constructor subclassnya
+         * merujuk kepada class Elemen superclassnya
+         */
+        this.nama = nama;
+    }
+
+}
